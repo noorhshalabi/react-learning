a4ffb1871553cc7b404e58f5525698bcaa4c44db
Noor Shalabi <noorhshalabi@gmail.com>
2024-02-20 21:37:27 -0600

    List Group: Dynanic Rendering minus css

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,17 +3,17 @@ function ListGroup() {
   //<h1>List</h1> //React.createElement('h1') --> to solve, grab entire expression, inside a div or another elemnt. Or select all code,
     //command palette, wrap with abbreviation <div>. But this adds extra element in dom. Avoid. USE fragment.
 
-    but shorter syntax, instead of important. Just add empty <> --> which defaults to react thinking to use default Fragment as wrapper
+    but shorter syntax, instead of import. Just add empty <> --> which defaults to react thinking to use default Fragment as wrapper
 */
+  const items = ["New York", "San Francisco", "Paris", "Austin"];
+  items.map((item) => <li>{item}</li>);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
