8014b18e5f8ee1b1f08095a569eefa61453ede87
Noor Shalabi <noorhshalabi@gmail.com>
2024-02-22 14:58:10 -0600

    Learning handling events, SyntheticBaseEvent, list group items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   /*
   //<h1>List</h1> //React.createElement('h1') --> to solve, grab entire expression, inside a div or another elemnt. Or select all code,
@@ -5,17 +6,36 @@ function ListGroup() {
 
     but shorter syntax, instead of import. Just add empty <> --> which defaults to react thinking to use default Fragment as wrapper
 */
-  const items = ["New York", "San Francisco", "Paris", "Austin"];
+  let items = ["New York", "San Francisco", "Paris", "Austin"];
   items.map((item) => <li>{item}</li>);
+
+  //event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
 }
 export default ListGroup;
+
+/*
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+
+    Lines above can be replaced with line 15, using { } you are able to render things conditionally inside jsx expression
+    Can only use html if not using { } braces
+*/
