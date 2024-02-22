7133603191ea5c398b24ca3ff0661f789964c71c
Noor Shalabi <noorhshalabi@gmail.com>
2024-02-22 16:39:38 -0600

    ReactNode understanding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  let items = ["New York", "San Francisco", "Paris", "Austin"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      {" "}
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />{" "}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
 
 export default App;
+
+//       <Alert text="Hello World"></Alert>
+// Above is Ugly - what if text is long. Use children
+// what if you want to pass html content? In props; change from string to ReactNode.
